2d98c366b310f13a780ee96c8d1b8b2bbb789d7a
Tigran Karakhanyan <lev.98@bk.ru>
2022-02-02 11:43:12 +0300

    Initial Commit

---
+++ MVP-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP-App
+//
+//  Created by Karakhanyan Tigran on 02.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
